5651c5b80eef0de15e2ea181de955067ae934890
Aleksandar V <aleks.mkd@gmail.com>
2021-03-01 22:55:18 +0000

    Some minor fixes

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -9,6 +9,9 @@ resource "aws_instance" "Udacity_T2" {
   instance_type = "t2.micro"
   count = 4
   subnet_id = var.vpc_public_subnets[0]
+  tags = {
+    Name = "Udacity_T2"
+  }
 }
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
@@ -17,4 +20,7 @@ resource "aws_instance" "Udacity_T2" {
 #   instance_type = "m4.large"
 #   count = 2
 #   subnet_id = var.vpc_public_subnets[0]
+#   tags = {
+#     Name = "Udacity_M4"
+#   }
 # }
